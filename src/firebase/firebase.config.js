ba1b06a626dd3ebe7425444d833e98632b4205c1
Irfan Chowdhury <irfanchowdhury80@gmail.com>
2022-11-25 11:03:47 +0600

    React Router install and Firebase Setup

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,27 +1,3 @@
-<div align='center'>
-
-# E-Recycle-Phone
-</div>
-
-
-
-## `PACKAGES SETUP`
-#### 1. Create [React](https://reactjs.org/) App
-```bash
-npx create-react-app your-project-name
-```
-
-#### 2. [React Router](https://reactrouter.com/en/main)
-```bash
-npm install react-router-dom
-```
-#### 3. [Firebase](https://console.firebase.google.com/) Setup
-```bash
-npm install firebase
-```
-and then create file `src/firebase/firebase.config.js` and copy-paste code
-
-```bash
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -40,6 +16,3 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
-```
-create `.env.local` file in your application and collect credentials from firebase and then set these in the env file
- 
